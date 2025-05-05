b8ecd2b701a24fbc3f47a0b18d1fff120431443c
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-05 15:44:10 +0700

    menambahkan pointer references

--- pertemuan8.cpp
+++ pertemuan8.cpp
@@ -12,3 +12,32 @@ class Staf{
     public:
     int nidn;
 };
+int main() {
+    Dosen ds;
+    ds.nama = "Giga";
+    ds.tampilNama();
+
+    Dosen &dsref = ds;
+    dsref.nama = "Joko";
+    cout << "Alamat memori = " << &dsref << endl;
+    dsref.tampilNama();
+
+    Dosen *psd = &ds;
+    psd->nama = "Reza";
+    cout << "Alamat memori = " << psd << endl;
+    psd->tampilNama();
+
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "Alamat memori a = " << &a << endl;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+    c = &b;
+    cout << "alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+}
